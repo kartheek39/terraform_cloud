8f512bd7bd5ed5410bc0e6c7d2a2f1ca26e3b6c5
kartheek Bodigam <bablutech39@gmail.com>
2025-04-16 12:38:33 +0530

    updated main tf file removed rg script

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "eastus"
-}
+# resource "azurerm_resource_group" "example" {
+#   name     = "example-resources"
+#   location = "eastus"
+# }
 
 resource "azurerm_storage_account" "example" {
   name                     = "mystorage3939"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = "rg-kartheek-practice-eastus"
+  location                 = "eastus"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
